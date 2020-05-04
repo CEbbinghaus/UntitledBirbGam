1d5db8aecdcef42d0ae2bd08cc5b3d85e73de53f
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-05-04 14:44:10 +1000

    First Prototype

---
+++ Assets/Scripts/testBird.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testBird : MonoBehaviour
+{
+    public float moveSpeed;
+    public float turnSpeed;
+    void FixedUpdate()
+    {
+        transform.position += transform.forward * moveSpeed * Time.fixedDeltaTime;
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.fixedDeltaTime, 0);
+    }
+}
