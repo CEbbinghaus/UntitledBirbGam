edd1dbe35065b10840b900af847a127663ca96fb
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-05-06 15:28:32 +1000

    Dynamic Camera Implementation

---
+++ Assets/Scripts/DynamicCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicCamera : MonoBehaviour
+{
+    CameraController c;
+
+    public List<Transform> searchItems;
+
+    public PlayerManager player;
+
+    public float magnetDistance = 20f;
+
+    private void Awake()
+    {
+        c = GetComponent<CameraController>();
+    }
+
+    void Update()
+    {
+        foreach (var item in searchItems)
+        {
+            print($"Distance to {item.name}: {Vector3.Distance(player.transform.position, item.position)}");
+            if (Vector3.Distance(player.transform.position, item.position) < magnetDistance && !c.Targets.Contains(item))
+            {
+                c.Targets.Add(item);
+            }
+            else
+            {
+                if (c.Targets.Contains(item))
+                {
+                    c.Targets.Remove(item);
+                }
+            }
+        }
+    }
+}
